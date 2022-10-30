6ab4d6438790c3216c027abd87dfe16634eb5894
Rafa <raphael.mota.marquez@gmail.com>
2022-10-30 20:58:44 +0100

    Core installation ionic and devextreme. Remove .ts

---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,43 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">UPU user list</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <UserList name="Tab 1 page" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import UserList from "@/components/business/UserList.vue";
+
+export default defineComponent({
+  name: "Tab1Page",
+  components: {
+    UserList,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+  },
+  methods: {},
+});
+</script>
---
+++ src/views/Tab2Page.vue
@@ -0,0 +1,75 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 2</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 2</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <DxChart :data-source="data">
+        <DxArgumentAxis :tick-interval="10" />
+        <DxSeries type="bar" />
+        <DxLegend :visible="false" />
+      </DxChart>
+      <ExploreContainer name="Tab 2 page" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import ExploreContainer from "@/components/ExploreContainer.vue";
+import DxChart, {
+  DxArgumentAxis,
+  DxSeries,
+  DxLegend,
+} from "devextreme-vue/chart";
+
+const data = [
+  {
+    arg: 1990,
+    val: 5320816667,
+  },
+  {
+    arg: 2000,
+    val: 6127700428,
+  },
+  {
+    arg: 2010,
+    val: 6916183482,
+  },
+];
+
+export default defineComponent({
+  name: "Tab2Page",
+  components: {
+    ExploreContainer,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    DxChart,
+    DxArgumentAxis,
+    DxSeries,
+    DxLegend,
+  },
+  data() {
+    return {
+      data,
+    };
+  },
+});
+</script>
